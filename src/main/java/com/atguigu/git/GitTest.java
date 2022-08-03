d67668fc59949871cad934744e716e3399229e05
ssj <1781654964@qq.com>
2022-08-03 09:38:48 +0800

    hot second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,8 @@ public class GitTest {
         System.out.println("你好，git2");
         System.out.println("你好，git3");
         System.out.println("你好，git4");
+        System.out.println("hotfix test");
+
 
     }
 }
