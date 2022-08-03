e8a2b8af604cfb10c6804d5f5ed48137270fb3ef
wwwssj <110469067+wwwssj@users.noreply.github.com>
2022-08-03 14:09:52 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hotfix test");
         System.out.println("ssh test");
+         System.out.println("pull test");
 
 
 
