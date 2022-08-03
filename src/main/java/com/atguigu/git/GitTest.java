b213ccf30a23ec567445a980f0021d3ef3150384
ssj <1781654964@qq.com>
2022-08-03 09:39:56 +0800

    master four commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("你好，git2");
         System.out.println("你好，git3");
         System.out.println("你好，git4");
+        System.out.println("master test");
 
     }
 }
