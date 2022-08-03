c653cf26b4574a2691636f5889b0473c31f893e9
ssj <1781654964@qq.com>
2022-08-03 09:02:31 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好，git");
         System.out.println("你好，git2");
+        System.out.println("你好，git3");
 
     }
 }
