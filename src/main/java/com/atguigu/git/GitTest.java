87504132fb3282c73c382038e78ab78ffea965c1
ssj <1781654964@qq.com>
2022-08-03 09:32:18 +0800

    hot first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("你好，git");
         System.out.println("你好，git2");
         System.out.println("你好，git3");
+        System.out.println("你好，git4");
 
     }
 }
