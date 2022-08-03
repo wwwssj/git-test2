80973d6c13f536bb9f30c4084f78a9c5fe67aed2
ssj <1781654964@qq.com>
2022-08-03 09:01:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author shi
+ * @date 2022-08-03 09:00
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("你好，git");
+    }
+}
+
